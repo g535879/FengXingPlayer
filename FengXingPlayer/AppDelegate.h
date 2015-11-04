ce3c11bf3b3d97fc5b123b4b46c707d70475fc73
g535879 <guyubinn@qq.com>
2015-11-04 17:48:54 +0800

    first Commit

---
+++ FengXingPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FengXingPlayer
+//
+//  Created by 古玉彬 on 15/11/4.
+//  Copyright © 2015年 guyubin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
